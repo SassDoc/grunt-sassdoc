f9d771378d92e9cd322494930918ad9a64dbc55f
Pascal Duez <pascal@duez.me>
2014-07-07 10:38:25 +0200

    chore(test): tests for both config options

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,18 +8,25 @@ module.exports = function (grunt) {
   // Time how long tasks take.
   require('time-grunt')(grunt);
 
+  var pkg = grunt.file.readJSON('package.json');
+
   grunt.initConfig({
 
     sassdoc: {
-      test: {
+      test_config: {
         src: 'test/stylesheets',
         dest: 'test/docs',
         options: {
-          config: 'test/view.json',
-          // Pass additional options.
-          // title: 'grunt-sassdoc test',
-          // version: false,
-          // display_access: ['public'],
+          config: 'test/view.json'
+        }
+      },
+      test_options: {
+        src: 'test/stylesheets',
+        dest: 'test/docs',
+        options: {
+          title: pkg.title || pkg.name,
+          version: 'v' + pkg.version,
+          display_access: ['public'],
           display_alias: true
         }
       }
@@ -46,10 +53,21 @@ module.exports = function (grunt) {
 
   grunt.loadTasks('tasks');
 
-  grunt.registerTask('test', [
-    'sassdoc:test',
+  grunt.registerTask('test_config', [
+    'sassdoc:test_config',
     'simplemocha:test',
     'clean:test'
   ]);
+
+  grunt.registerTask('test_options', [
+    'sassdoc:test_options',
+    'simplemocha:test',
+    'clean:test'
+  ]);
+
+  grunt.registerTask('test', function () {
+    // https://github.com/yaymukund/grunt-simple-mocha/pull/30
+    grunt.task.run(['test_config', 'test_options']);
+  });
 
 };
