0b078720d4090ce771da0401d9d9cd1282cdbe96
Pascal Duez <public@pascalduez.me>
2016-02-21 12:32:38 +0100

    Update unit tests to follow default SassDoc behaviour

--- test/sassdoc.test.js
+++ test/sassdoc.test.js
@@ -4,22 +4,23 @@ var fs = require('fs');
 var path = require('path');
 var test = require('tape');
 
+var docsDir = path.resolve(process.cwd(), 'sassdoc');
+
 test('sassdoc', function (assert) {
   assert.plan(3);
 
   assert.ok(
-    fs.existsSync(path.resolve(__dirname, 'sassdoc')),
+    fs.existsSync(docsDir),
     'Should create a `sassdoc` dir'
   );
 
   assert.ok(
-    fs.existsSync(path.resolve(__dirname, 'sassdoc/index.html')),
+    fs.existsSync(path.join(docsDir, 'index.html')),
     'Should create SassDocs index'
   );
 
   assert.ok(
-    fs.existsSync(path.resolve(__dirname, 'sassdoc/assets')),
+    fs.existsSync(path.join(docsDir, 'assets')),
     'Should dump SassDocs assets'
   );
-
 });
